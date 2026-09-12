740f53b7bd62068ce1f0d0297d11cc8945d91b0d
taeha9309-create <taeha9309@gmail.com>
2026-09-12 12:01:18 +0900

    chore: configure CRXJS extension environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from "./manifest.config.ts";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    crx({ manifest }),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
